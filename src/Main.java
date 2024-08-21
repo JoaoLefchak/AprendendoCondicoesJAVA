20000cc454fcfc18b796c2cbf4c9ce6d4d732059
Lefchakk <joao.lefchak@edu.unipar.br>
2024-08-21 20:51:46 -0300

    Criação do projeto Java

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,30 @@
+import java.util.function.DoubleToIntFunction;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
-        System.out.printf("Hi, my name is John!");
+        Integer idadePessoa = 20;
+        String nomePessoa = "João Gabriel";
+
+        /* Compara textos
+        * >  Maior
+        * >= Maior igual
+        * <  Menor
+        * <= Menor igual
+        * == É igual
+        * != É diferente
+         */
+
+        if(idadePessoa < 21) {
+            System.out.println("É maior que 21!");
+        }else{
+            System.out.println("Não é maior que 21!");
+
+        }
+
+
 
 
         }
